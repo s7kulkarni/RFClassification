e72772f94f9e586b108ff9f588d9c626baad549c
Shriniwas-kulkarni9 <shrinivask.2k@gmail.com>
2025-03-22 22:23:15 -0700

    PERTURBATION code

--- dronerf_feat_incremental.py
+++ dronerf_feat_incremental.py
@@ -12,7 +12,7 @@ from feat_gen_functions import *
 t_seg = 250
 fs = 40e6 #40 MHz
 features_folder = dronerf_feat_path
-n_per_seg = 8192 # length of each segment (powers of 2)
+n_per_seg = 4096 # length of each segment (powers of 2)
 n_overlap_spec = 120
 win_type = 'hamming' # make ends of each segment match
 high_low = 'L' #'L', 'H' # high or low range of frequency
