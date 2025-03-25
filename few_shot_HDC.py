780a55177f79bd50d1bcb0077b551946d9419ff8
Shriniwas-kulkarni9 <shrinivask.2k@gmail.com>
2025-03-25 16:31:22 -0700

    SVM , HDC perturbed 001

--- few_shot_HDC.py
+++ few_shot_HDC.py
@@ -132,7 +132,7 @@ t_seg = 250 #ms
 n_per_seg = 4096
 interferences = ['WIFI', 'BLUE', 'BOTH', 'CLEAN']
 output_name = 'drones'
-norm_ratio = '05' # 0.xxx mapped to xxx
+norm_ratio = '001' # 0.xxx mapped to xxx
 feat_format = 'ARR'
 which_dataset = 'dronerf'
 output_tensor = False
@@ -234,7 +234,7 @@ for fold, (train_idx, test_idx) in enumerate(skf.split(X_tensor, Y_tensor)):
 
     # Create DataLoader for training and testing
     train_dataset = torch.utils.data.TensorDataset(X_train, Y_train)
-    test_dataset = torch.utils.data.TensorDataset(X_test, Y_test)
+    test_dataset = torch.utils.data.TensorDataset(X_perturbed[test_idx], Y_perturbed[test_idx])
 
     train_ld = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
     test_ld = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
