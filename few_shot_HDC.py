f493e37ab5213fc3373b334c4831ec742ba396d7
Shriniwas-kulkarni9 <shrinivask.2k@gmail.com>
2025-04-01 23:27:32 -0700

    TOBOREVERTED HARDCODING IN PERTURBED DATASET

--- few_shot_HDC.py
+++ few_shot_HDC.py
@@ -200,9 +200,9 @@ Y_int = label_encoder.fit_transform(Y)
 X_tensor = torch.tensor(X, dtype=torch.float32)  # (219, 4097)
 Y_tensor = torch.tensor(Y_int, dtype=torch.long)  # (219,)
 
-Y_perturbed = label_encoder.fit_transform(Y_perturbed)
-X_perturbed = torch.tensor(X_perturbed, dtype=torch.float32)  # (219, 4097)
-Y_perturbed = torch.tensor(Y_perturbed, dtype=torch.long)  # (219,)
+# Y_perturbed = label_encoder.fit_transform(Y_perturbed)
+# X_perturbed = torch.tensor(X_perturbed, dtype=torch.float32)  # (219, 4097)
+# Y_perturbed = torch.tensor(Y_perturbed, dtype=torch.long)  # (219,)
 
 # K-Fold Cross-Validation
 k_folds = 5  # You can change this
