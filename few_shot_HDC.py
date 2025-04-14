4407009a35b886d005cae1ffb9cdeb46f8774a70
Shriniwas-kulkarni9 <shrinivask.2k@gmail.com>
2025-04-14 12:12:16 -0700

    few shot 4 prec dft

--- few_shot_HDC.py
+++ few_shot_HDC.py
@@ -201,7 +201,7 @@ output_tensor = False
 in_features = 2049
 DIMENSIONS = 10000
 seed = 86
-perturbation_type = 'random'
+perturbation_type = 'dft_attack'
 
 print('Loading DroneRF Dataset')
 highlow = 'L'
