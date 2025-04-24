9c59899b0109a13159004f80ab2c8713c5dd3796
Shriniwas-kulkarni9 <shrinivask.2k@gmail.com>
2025-04-23 18:12:54 -0700

    few shot svm

--- ml_approaches.py
+++ ml_approaches.py
@@ -140,8 +140,8 @@ print(np.sum(y_use=='None'))
 
 model = PsdSVM(t_seg, n_per_seg)
 
-# accs, f1s, runts = model.run_cv(X_use, y_use, k_fold=5)
-accs, f1s, runts = model.run_cv_perturbed(X_use, y_use, X_perturbed, y_perturbed, k_fold=5)
+accs, f1s, runts = model.run_cv(X_use, y_use, k_fold=5)
+# accs, f1s, runts = model.run_cv_perturbed(X_use, y_use, X_perturbed, y_perturbed, k_fold=5)
 
 for icv in range(5):
     print(model.cv_models[icv].support_vectors_.shape)
